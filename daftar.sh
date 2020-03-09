89637a211b13537262e83512d8780fe2b2029d88
RAJABING <59326452+RAJABING@users.noreply.github.com>
2020-03-09 16:46:56 +0700

    Daftar
    Yay

---
+++ daftar.sh
@@ -0,0 +1,17 @@
+clear
+echo "================"
+echo $red "1.) BINGKU|"
+echo "================"
+read -p "PILIH TOOLS UNTUK MASUK : " tuan
+
+
+if [ $tuan = 1] || [ $tuan = 1]
+then
+clear
+git clone https://github.com/RAJABING/bingku
+ls
+ls bingku
+cd bingku
+ls bingku
+sh bingku.sh
+fi
